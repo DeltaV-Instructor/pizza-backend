9a69dc00b0b2e2efd30805c69870b32207c28221
tektechnologies <info@tektechnologies.com>
2023-01-28 11:59:07 -0600

    add data and route for pizza

--- server.js
+++ server.js
@@ -16,11 +16,13 @@
 // 1.
 const express = require('express');
 require('dotenv').config();
+let data = require('./data/pizza.json');
 // 2.
 const app = express();
 
 
 const PORT = process.env.PORT || 5005;
+
 
 
 // routes
@@ -40,18 +42,54 @@ app.get('/',(request, response)=>{
 
 app.get('/hello', (request, response) => {
   //localhost:3003/hello?name=bob&lastname=trapper
-  console.log('request object', request.query.name, request.query.lastname);
-  
-  response.status(200).send('hello');
+  console.log('request object', request.query);
+  let firstName = request.query.name;
+  let lastName = request.query.lastname;
+  console.log('!!!1', firstName, lastName);
+  response.status(200).send(`Hello, ${firstName} ${lastName}`);
+});
+
+app.get('/pizza', (request, response) => {
+  //http://localhost:3003/pizza?pizzatype=Chicago%20Pizza
+  try {
+    let pizzaType = request.query.pizzatype;
+    // let dataToSend = data.find(pizza => pizza.pizzatype === pizzaType);
+    // console.log('did we find pizza type?', dataToSend);
+    let dataToInstantiate = data.find(pizza => pizza.pizzatype === pizzaType);
+    let dataToSend = new Pizza(dataToInstantiate);
+    console.log(dataToSend, 'got back from Pizza class');
+    response.send(dataToSend);
+  } catch (error) {
+    //create a new instance of error
+    //this will instantiate any new error
+    // eslint-disable-next-line no-undef
+    next(error);
+  }
 });
 
 
 
 
+app.get('*', (request, response)=>{
+  response.status(404).send('The route was not found. Error 404');
+});
+
 // class
+class Pizza {
+  constructor(pizzaObject){
+    console.log('did we get an object to construct?', pizzaObject);
+    //do some stuff to it
+    this.pizzaType = pizzaObject.pizzatype;
+    this.location = pizzaObject.location;
+  }
+}
 
+//Errors
 
-// errorhandling
+// eslint-disable-next-line no-unused-vars
+app.use((error, request, response, next) => {
+  response.status(500).send(error.message);
+});
 
 // Listen for PORT to start and keep our server running
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
